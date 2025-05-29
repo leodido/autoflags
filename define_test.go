086d5ddd70ebaaeb072472e0553e25c5d2c98bcc
Leonardo Di Donato <leodidonato@gmail.com>
2025-05-29 15:40:28 +0200

    test: prepare test cases for int and uint flags
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- define_test.go
+++ define_test.go
@@ -19,6 +19,31 @@ func TestFlagsBaseSuite(t *testing.T) {
 	suite.Run(t, new(FlagsBaseSuite))
 }
 
+type ConfigFlags struct {
+	LogLevel string `default:"info" flag:"log-level" flagdescr:"set the logging level" flaggroup:"Config"`
+	Timeout  int    `flagdescr:"set the timeout, in seconds" flagset:"Config"`
+	Endpoint string `flagdescr:"the listen.dev endpoint emitting the verdicts" flaggroup:"Config" flagrequired:"true"`
+}
+
+type DeepFlags struct {
+	Deep time.Duration `default:"deepdown" flagdescr:"deep flag" flag:"deep" flagshort:"d" flaggroup:"Deep"`
+}
+
+type JSONFlags struct {
+	JSON bool      `flagdescr:"output the verdicts (if any) in JSON form"`
+	JQ   string    `flagshort:"q" flagdescr:"filter the output using a jq expression"`
+	Deep DeepFlags `flagrequired:"true"`
+}
+
+type testOptions struct {
+	ConfigFlags `flaggroup:"Configuration"`
+	Nest        JSONFlags
+}
+
+func (o testOptions) Attach(c *cobra.Command)             {}
+func (o testOptions) Transform(ctx context.Context) error { return nil }
+func (o testOptions) Validate() []error                   { return nil }
+
 func (suite *FlagsBaseSuite) TestDefine() {
 	cases := []struct {
 		desc  string
@@ -83,27 +108,132 @@ func (suite *FlagsBaseSuite) TestDefine() {
 	}
 }
 
-type ConfigFlags struct {
-	LogLevel string `default:"info" flag:"log-level" flagdescr:"set the logging level" flaggroup:"Config"`
-	Timeout  int    `flagdescr:"set the timeout, in seconds" flagset:"Config"`
-	Endpoint string `flagdescr:"the listen.dev endpoint emitting the verdicts" flaggroup:"Config" flagrequired:"true"`
+type uintTestOptions struct {
+	UintField   uint   `flag:"uint-field" flagdescr:"test uint field"`
+	Uint8Field  uint8  `flag:"uint8-field" flagdescr:"test uint8 field"`
+	Uint16Field uint16 `flag:"uint16-field" flagdescr:"test uint16 field"`
+	Uint32Field uint32 `flag:"uint32-field" flagdescr:"test uint32 field"`
+	Uint64Field uint64 `flag:"uint64-field" flagdescr:"test uint64 field"`
 }
 
-type DeepFlags struct {
-	Deep time.Duration `default:"deepdown" flagdescr:"deep flag" flag:"deep" flagshort:"d" flaggroup:"Deep"`
+func (o uintTestOptions) Attach(c *cobra.Command)             {}
+func (o uintTestOptions) Transform(ctx context.Context) error { return nil }
+func (o uintTestOptions) Validate() []error                   { return nil }
+
+func (suite *FlagsBaseSuite) TestDefine_UintTypesSupport() {
+	opts := &uintTestOptions{
+		UintField:   500,
+		Uint8Field:  50,
+		Uint16Field: 1000,
+		Uint32Field: 100000,
+		Uint64Field: 10000000000,
+	}
+	cmd := &cobra.Command{}
+
+	Define(cmd, opts)
+
+	// Test uint
+	flagUint := cmd.Flags().Lookup("uint-field")
+	assert.NotNil(suite.T(), flagUint, "uint flag should be created")
+
+	err := cmd.Flags().Set("uint-field", "1500")
+	assert.NoError(suite.T(), err, "should be able to set uint flag")
+	assert.Equal(suite.T(), uint(1500), opts.UintField, "uint struct field should be updated")
+
+	// Test uint8
+	flagUint8 := cmd.Flags().Lookup("uint8-field")
+	assert.NotNil(suite.T(), flagUint8, "uint8 flag should be created")
+
+	err = cmd.Flags().Set("uint8-field", "100")
+	assert.NoError(suite.T(), err, "should be able to set uint8 flag")
+	assert.Equal(suite.T(), uint8(100), opts.Uint8Field, "uint8 struct field should be updated")
+
+	// Test uint16
+	flag16 := cmd.Flags().Lookup("uint16-field")
+	assert.NotNil(suite.T(), flag16, "uint16 flag should be created")
+
+	err = cmd.Flags().Set("uint16-field", "2000")
+	assert.NoError(suite.T(), err, "should be able to set uint16 flag")
+	assert.Equal(suite.T(), uint16(2000), opts.Uint16Field, "uint16 struct field should be updated")
+
+	// Test uint32
+	flag32 := cmd.Flags().Lookup("uint32-field")
+	assert.NotNil(suite.T(), flag32, "uint32 flag should be created")
+
+	err = cmd.Flags().Set("uint32-field", "200000")
+	assert.NoError(suite.T(), err, "should be able to set uint32 flag")
+	assert.Equal(suite.T(), uint32(200000), opts.Uint32Field, "uint32 struct field should be updated")
+
+	// Test uint64
+	flag64 := cmd.Flags().Lookup("uint64-field")
+	assert.NotNil(suite.T(), flag64, "uint64 flag should be created")
+
+	err = cmd.Flags().Set("uint64-field", "20000000000")
+	assert.NoError(suite.T(), err, "should be able to set uint64 flag")
+	assert.Equal(suite.T(), uint64(20000000000), opts.Uint64Field, "uint64 struct field should be updated")
 }
 
-type JSONFlags struct {
-	JSON bool      `flagdescr:"output the verdicts (if any) in JSON form"`
-	JQ   string    `flagshort:"q" flagdescr:"filter the output using a jq expression"`
-	Deep DeepFlags `flagrequired:"true"`
+type intTestOptions struct {
+	IntField   int   `flag:"int-field" flagdescr:"test int field"`
+	Int8Field  int8  `flag:"int8-field" flagdescr:"test int8 field"`
+	Int16Field int16 `flag:"int16-field" flagdescr:"test int16 field"`
+	Int32Field int32 `flag:"int32-field" flagdescr:"test int32 field"`
+	Int64Field int64 `flag:"int64-field" flagdescr:"test int64 field"`
 }
 
-type testOptions struct {
-	ConfigFlags `flaggroup:"Configuration"`
-	Nest        JSONFlags
-}
+func (o intTestOptions) Attach(c *cobra.Command)             {}
+func (o intTestOptions) Transform(ctx context.Context) error { return nil }
+func (o intTestOptions) Validate() []error                   { return nil }
 
-func (o testOptions) Attach(c *cobra.Command)             {}
-func (o testOptions) Transform(ctx context.Context) error { return nil }
-func (o testOptions) Validate() []error                   { return nil }
+func (suite *FlagsBaseSuite) TestDefine_IntTypesSupport() {
+	opts := &intTestOptions{
+		IntField:   1000,
+		Int8Field:  42,
+		Int16Field: 1234,
+		Int32Field: 123456,
+		Int64Field: 1234567890,
+	}
+	cmd := &cobra.Command{}
+
+	Define(cmd, opts)
+
+	// Test int
+	flagInt := cmd.Flags().Lookup("int-field")
+	assert.NotNil(suite.T(), flagInt, "int flag should be created")
+
+	err := cmd.Flags().Set("int-field", "2000")
+	assert.NoError(suite.T(), err, "should be able to set int flag")
+	assert.Equal(suite.T(), int(2000), opts.IntField, "int struct field should be updated")
+
+	// Test int8
+	flagInt8 := cmd.Flags().Lookup("int8-field")
+	assert.NotNil(suite.T(), flagInt8, "int8 flag should be created")
+
+	err = cmd.Flags().Set("int8-field", "100")
+	assert.NoError(suite.T(), err, "should be able to set int8 flag")
+	assert.Equal(suite.T(), int8(100), opts.Int8Field, "int8 struct field should be updated")
+
+	// Test int16
+	flagInt16 := cmd.Flags().Lookup("int16-field")
+	assert.NotNil(suite.T(), flagInt16, "int16 flag should be created")
+
+	err = cmd.Flags().Set("int16-field", "5678")
+	assert.NoError(suite.T(), err, "should be able to set int16 flag")
+	assert.Equal(suite.T(), int16(5678), opts.Int16Field, "int16 struct field should be updated")
+
+	// Test int32
+	flagInt32 := cmd.Flags().Lookup("int32-field")
+	assert.NotNil(suite.T(), flagInt32, "int32 flag should be created")
+
+	err = cmd.Flags().Set("int32-field", "987654")
+	assert.NoError(suite.T(), err, "should be able to set int32 flag")
+	assert.Equal(suite.T(), int32(987654), opts.Int32Field, "int32 struct field should be updated")
+
+	// Test int64
+	flagInt64 := cmd.Flags().Lookup("int64-field")
+	assert.NotNil(suite.T(), flagInt64, "int64 flag should be created")
+
+	err = cmd.Flags().Set("int64-field", "9876543210")
+	assert.NoError(suite.T(), err, "should be able to set int64 flag")
+	assert.Equal(suite.T(), int64(9876543210), opts.Int64Field, "int64 struct field should be updated")
+}
