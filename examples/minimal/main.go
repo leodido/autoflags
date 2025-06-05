14eeb5d8bfff4d8477d44e40c1ec34306b500f7b
Leonardo Di Donato <leodidonato@gmail.com>
2025-06-06 01:03:07 +0200

    docs: create minimal example
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- examples/minimal/main.go
+++ examples/minimal/main.go
@@ -1,4 +1,38 @@
 package main
 
-// TODO: impl
-func main() {}
+import (
+	"fmt"
+
+	"github.com/leodido/autoflags"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap/zapcore"
+)
+
+type Options struct {
+	LogLevel zapcore.Level
+	Port     int
+}
+
+func (o *Options) Attach(c *cobra.Command) {
+	autoflags.Define(c, o)
+}
+
+func main() {
+	opts := &Options{}
+	cli := &cobra.Command{Use: "myapp"}
+
+	// This single line creates all the options (flags, env vars)
+	opts.Attach(cli)
+
+	cli.PreRunE = func(c *cobra.Command, args []string) error {
+		return autoflags.Unmarshal(c, opts) // Populates struct from flags
+	}
+
+	cli.RunE = func(c *cobra.Command, args []string) error {
+		fmt.Println(opts)
+
+		return nil
+	}
+
+	cli.Execute()
+}
