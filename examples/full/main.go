0c81ab9b8fac989c483687f901ac6f3a7f4d965c
Leonardo Di Donato <leodidonato@gmail.com>
2025-06-14 15:11:11 +0200

    docs(examples): refine examples logging
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- examples/full/main.go
+++ examples/full/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"os"
+	"log"
 
 	full_example_cli "github.com/leodido/autoflags/examples/full/cli"
 )
 
 func main() {
+	log.SetFlags(0)
 	c := full_example_cli.NewRootC()
+
 	if err := c.Execute(); err != nil {
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 }
